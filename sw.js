065261a8e6003842de6855d937da8fa22654f4a9
ReaperChico <denizli2066@live.at>
2026-09-17 11:33:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'abenteurergilde-v3-1';
+const CACHE = 'abenteurergilde-v3-2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
